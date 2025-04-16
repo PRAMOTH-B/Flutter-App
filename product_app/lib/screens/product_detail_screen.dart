7c08a0cc14b453cf1e545e93513ef11e2c00f84e
PRAMOTH B <pramoth0208@gmail.com>
2025-04-16 13:03:02 +0530

    Intial Commit

--- product_app/lib/screens/product_detail_screen.dart
+++ product_app/lib/screens/product_detail_screen.dart
@@ -7,40 +7,51 @@ class ProductDetailScreen extends StatelessWidget {
   final String productId;
   const ProductDetailScreen({super.key, required this.productId});
 
-  @override
-  Widget build(BuildContext context) {
-    final product = Provider.of<ProductProvider>(context).getById(productId);
+ @override
+Widget build(BuildContext context) {
+  final product = Provider.of<ProductProvider>(context).getById(productId);
 
+  if (product == null) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Product Details'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.edit),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => EditProductScreen(product: product),
-                ),
-              );
-            },
-          )
-        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Name: ${product.name}', style: const TextStyle(fontSize: 20)),
-            const SizedBox(height: 10),
-            Text('Price: ₹ ${product.price.toStringAsFixed(2)}', style: const TextStyle(fontSize: 18)),
-            const SizedBox(height: 10),
-            Text('Description:\n${product.description}', style: const TextStyle(fontSize: 16)),
-          ],
-        ),
+      body: const Center(
+        child: Text('Product not found'),
       ),
     );
   }
+
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('Product Details'),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.edit),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => EditProductScreen(product: product),
+              ),
+            );
+          },
+        )
+      ],
+    ),
+    body: Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Name: ${product.name}', style: const TextStyle(fontSize: 20)),
+          const SizedBox(height: 10),
+          Text('Price: ₹ ${product.price.toStringAsFixed(2)}', style: const TextStyle(fontSize: 18)),
+          const SizedBox(height: 10),
+          Text('Description:\n${product.description}', style: const TextStyle(fontSize: 16)),
+        ],
+      ),
+    ),
+  );
+}
 }
